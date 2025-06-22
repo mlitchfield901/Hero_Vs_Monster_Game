1ada2d5915bb35d6d941ea6e9f0198ecd53d0c9c
Matt Litchfield <mlitchfield901@gmail.com>
2025-06-22 14:19:19 -0400

    Update Hero_Vs_Monster.cs
    Added comments to code to outline the desired function of the subsequent lines of code.

--- Hero_Vs_Monster.cs
+++ Hero_Vs_Monster.cs
@@ -1,22 +1,30 @@
+//variale initialization
 Random attack = new Random();
 int damage = 0;
 int heroHealth = 10;
 int monsterHealth = 10;
 
+// loop creation in order to continue the battle until a winner is determined
 do
 {
+    // determine damage to monster using a random number generated between the values of 1 through 10)
     damage = attack.Next(1, 11);
     monsterHealth = monsterHealth - damage;
     Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health.");
+    
+    // Determine if Monster is defeated
     if (monsterHealth <= 0)
     {
         Console.WriteLine("Hero wins!");
     }
     else
     {
+        // determine damage to hero using a random number generated between the values of 1 through 10)
         damage = attack.Next(1, 11);
         heroHealth = heroHealth - damage;
         Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health.");
+        
+        //Determine if ero is defeated
         if (heroHealth <= 0)
         {
             Console.WriteLine("Monster wins!");
